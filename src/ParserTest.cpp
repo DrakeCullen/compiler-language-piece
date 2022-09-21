b7c3e52ddc159a905702b3715f073c33f9ba7f26
DrakeCullen <dpcullen@mavs.coloradomesa.edu>
2022-09-20 19:09:34 -0600

    clean up testing

--- src/ParserTest.cpp
+++ src/ParserTest.cpp
@@ -10,16 +10,11 @@ ParserTest::ParserTest(string newFilename, int numInstances, testType type, bool
 bool ParserTest::doTest() {
 		int count=0,i=0;
 		while (p->peek().type!=END_OF_TEXT && i < 20) {
-            if (type==VARIABLE_TEST) 
-                if (p->variable(p->getTokenizer())) count++;
-            if (type==FRACTION_TEST) 
-                if (p->fraction(p->getTokenizer())) count++;
-            if (type==POINTFLOAT_TEST) 
-                if (p->pointFloat(p->getTokenizer())) count++;
-            if (type==EXPONENT_TEST)
-                if (p->exponent(p->getTokenizer())) count++;
-            if (type==EXPONENT_FLOAT_TEST)
-                if (p->exponentFloat(p->getTokenizer())) count++;
+            if (type==VARIABLE_TEST && p->variable(p->getTokenizer())) count++;
+            if (type==FRACTION_TEST && p->fraction(p->getTokenizer())) count++;
+            if (type==POINTFLOAT_TEST && p->pointFloat(p->getTokenizer())) count++;
+            if (type==EXPONENT_TEST && p->exponent(p->getTokenizer())) count++;
+            if (type==EXPONENT_FLOAT_TEST && p->exponentFloat(p->getTokenizer())) count++;
             i++;
         }
 		return count==numInstances;
